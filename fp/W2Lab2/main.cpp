317c388e0b7db0b95fb9526ec55ff00588e50d46
ArqamWaheed <arqam.waheed.dev@gmail.com>
2025-09-19 11:28:32 +0500

    Task 5 Done

--- fp/W2Lab2/main.cpp
+++ fp/W2Lab2/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 int main()  {
-    cout << "I am learning C++! \n";
-    cout << "This string \'\' is an empty message \n";
-    cout << "\\'\'\"\\\\\\\"\n";
-    cout << "\\\\\t1,\t 2,\t 3, \\\\\'\\\\\n";
-    return 0;
+    cout <<"+---------------------------------------------+" << endl;
+    cout <<"| Pakistan Post                        ###### |"<< endl;
+    cout <<"|                                      ###### |"<< endl;
+    cout <<"|            Anis Rahman               ###### |"<< endl;
+    cout <<"|            IAEC 204, NUST-SEECS      ###### |"<< endl;
+    cout <<"|            Islamabad, Pakistan              |"<< endl;
+    cout <<"|                 _ _ _ _ _                   |"<< endl;
+    cout <<"|                |_|_|_|_|_|                  |"<< endl;
+    cout <<"|                Postal Code                  |"<< endl;
+    cout <<"+---------------------------------------------+" << endl;
 }
