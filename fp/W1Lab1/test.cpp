d24c692e62ee8becf2ffa17c1afbde15e81f1db7
ArqamWaheed <arqam.waheed.dev@gmail.com>
2025-09-19 10:44:22 +0500

    Lab1 Finished

---
+++ fp/W1Lab1/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    std::cout << "      3      ";
+    std::cout << "\n     333     ";
+    std::cout << "\n    33333    ";
+    std::cout << "\n   3333333   ";
+    std::cout << "\n  333333333  ";
+
+    return 0;
+}
---
+++ fp/W2Lab2/main.cpp
@@ -0,0 +1,14 @@
+// Arqam Waheed, September 19, 2025
+// Yes i thought that // creates comments and i was right about it.
+
+#include <iostream>
+
+// Single line comment 
+int main() {
+    /* This 
+    is a block comment*/
+    std::cout << "Hello world\n"; // Prints hello world
+
+    std::cout << "I am a C++ program"; // Prints new statemnet
+    return EXIT_SUCCESS;
+}
