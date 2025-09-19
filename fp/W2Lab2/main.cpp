6b4743110feb77537c9e1c6c0a3a7146ce81c124
ArqamWaheed <arqam.waheed.dev@gmail.com>
2025-09-19 12:04:28 +0500

    Task 6

--- fp/W2Lab2/main.cpp
+++ fp/W2Lab2/main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 using namespace std;
 int main()  {
-    cout <<"+---------------------------------------------+" << endl;
-    cout <<"| Pakistan Post                        ###### |"<< endl;
-    cout <<"|                                      ###### |"<< endl;
-    cout <<"|            Anis Rahman               ###### |"<< endl;
-    cout <<"|            IAEC 204, NUST-SEECS      ###### |"<< endl;
-    cout <<"|            Islamabad, Pakistan              |"<< endl;
-    cout <<"|                 _ _ _ _ _                   |"<< endl;
-    cout <<"|                |_|_|_|_|_|                  |"<< endl;
-    cout <<"|                Postal Code                  |"<< endl;
-    cout <<"+---------------------------------------------+" << endl;
+    cout <<" *********         ***           *           *      " << endl;
+    cout <<" *       *      *       *       ***         * *     "<< endl;
+    cout <<" *       *     *         *     *****       *   *    "<< endl;
+    cout <<" *       *     *         *       *        *     *   "<< endl;
+    cout <<" *       *     *         *       *       *       *  "<< endl;
+    cout <<" *       *     *         *       *        *     *   "<< endl;
+    cout <<" *       *     *         *       *         *   *    "<< endl;
+    cout <<" *       *      *       *        *          * *     "<< endl;
+    cout <<" *********         ***           *           *      "<< endl;
 }
