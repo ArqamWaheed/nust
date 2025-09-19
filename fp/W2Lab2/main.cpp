f65cfebfe4d7fc5673e0f6d44849ce150b18e367
ArqamWaheed <arqam.waheed.dev@gmail.com>
2025-09-19 11:15:18 +0500

    Task 3

--- fp/W2Lab2/main.cpp
+++ fp/W2Lab2/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 using namespace std;
 int main()  {
-    cout << "A \"quoted\" String is\n";
-    cout << "`much` better if you learn\n";
-    cout << "the rules of \"escape sequences.\"\n";
-    cout << "\n";
-    cout << "Also, \"\" represents an empty String.\n";
-    cout << "Don't forget: use \\\" instead of \" !\n";
-    cout << "'' is not the same as \"\n";
+    cout << "I am learning C++! \n";
+    cout << "This string \'\' is an empty message \n";
+    cout << "\\'\'\"\\\\\\\"\n";
+    cout << "\\\\\t1,\t 2,\t 3, \\\\\'\\\\\n";
+    return 0;
 }
