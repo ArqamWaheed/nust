fb78b8a98f16443eebbacf279f2c73b9e794a923
ArqamWaheed <arqam.waheed.dev@gmail.com>
2025-09-19 12:11:27 +0500

    Task 7, end of week 2

--- fp/W2Lab2/main.cpp
+++ fp/W2Lab2/main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 using namespace std;
-int main()  {
-    cout <<" *********         ***           *           *      " << endl;
-    cout <<" *       *      *       *       ***         * *     "<< endl;
-    cout <<" *       *     *         *     *****       *   *    "<< endl;
-    cout <<" *       *     *         *       *        *     *   "<< endl;
-    cout <<" *       *     *         *       *       *       *  "<< endl;
-    cout <<" *       *     *         *       *        *     *   "<< endl;
-    cout <<" *       *     *         *       *         *   *    "<< endl;
-    cout <<" *       *      *       *        *          * *     "<< endl;
-    cout <<" *********         ***           *           *      "<< endl;
+
+int main() {
+    cout << "+---------------------------------------------+" << endl;
+    cout << "| Arqam Tutoring Service               ###### |" << endl;
+    cout << "|                                      ###### |" << endl;
+    cout << "|            Arqam Waheed              ###### |" << endl;
+    cout << "|            arqamtutoring@gmail.com   ###### |" << endl;
+    cout << "|            Islamabad, Pakistan              |" << endl;
+    cout << "|                                             |" << endl;
+    cout << "|             +---------------------+         |" << endl;
+    cout << "|             |                     |         |" << endl;  
+    cout << "|             |                     |         |" << endl;  
+    cout << "|             |       [PHOTO]       |         |" << endl;
+    cout << "|             |                     |         |" << endl;
+    cout << "|             |                     |         |" << endl;  
+    cout << "|             +---------------------+         |" << endl;
+    cout << "+---------------------------------------------+" << endl;
 }
