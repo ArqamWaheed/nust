1c9d895974a777c075a1757408d1cb896e00968b
ArqamWaheed <arqam.waheed.dev@gmail.com>
2025-09-19 11:11:06 +0500

    Task 4 done

--- fp/W2Lab2/main.cpp
+++ fp/W2Lab2/main.cpp
@@ -1,14 +1,11 @@
-// Arqam Waheed, September 19, 2025
-// Yes i thought that // creates comments and i was right about it.
-
 #include <iostream>
-
-// Single line comment 
-int main() {
-    /* This 
-    is a block comment*/
-    std::cout << "Hello world\n"; // Prints hello world
-
-    std::cout << "I am a C++ program"; // Prints new statemnet
-    return EXIT_SUCCESS;
+using namespace std;
+int main()  {
+    cout << "A \"quoted\" String is\n";
+    cout << "`much` better if you learn\n";
+    cout << "the rules of \"escape sequences.\"\n";
+    cout << "\n";
+    cout << "Also, \"\" represents an empty String.\n";
+    cout << "Don't forget: use \\\" instead of \" !\n";
+    cout << "'' is not the same as \"\n";
 }
