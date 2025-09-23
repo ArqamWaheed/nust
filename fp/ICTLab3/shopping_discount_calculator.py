7434147746e8a6b06ec85a989f2f568a407c7b5a
ArqamWaheed <arqam.waheed.dev@gmail.com>
2025-09-23 14:30:44 +0500

    Task 2

---
+++ fp/ICTLab3/shopping_discount_calculator.py
@@ -0,0 +1,6 @@
+name = "Arqam Waheed"
+cart_value = 1000 
+discount = 10 # this is percentage
+tax = 8 # this is percentage
+
+print(f"Name: {name} \nCartValue: {cart_value} \nDiscount: {discount}% \nTax: {tax}%")
