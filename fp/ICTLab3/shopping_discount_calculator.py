247b32f8f1dad14deada9e3d7e6f409c8c465e55
ArqamWaheed <arqam.waheed.dev@gmail.com>
2025-09-23 14:40:00 +0500

    Task 3 completed

--- fp/ICTLab3/shopping_discount_calculator.py
+++ fp/ICTLab3/shopping_discount_calculator.py
@@ -1,6 +1,15 @@
+# Task 1
+print("Welcome to Python Lab 3!") 
+# In the output I am seeing what i have written inside the print statement as a string
+
+# Task 2
 name = "Arqam Waheed"
 cart_value = 1000 
 discount = 10 # this is percentage
 tax = 8 # this is percentage
-
 print(f"Name: {name} \nCartValue: {cart_value} \nDiscount: {discount}% \nTax: {tax}%")
+
+# Task 3
+discount_amount = cart_value * discount / 100
+tax_amount = cart_value * tax / 100
+final_value = cart_value - discount_amount + tax_amount
