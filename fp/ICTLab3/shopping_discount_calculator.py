8cc71d5caa3c4212c34a225b3ed40b0aec7d1c42
ArqamWaheed <arqam.waheed.dev@gmail.com>
2025-09-23 14:44:41 +0500

    Task 4 completed

--- fp/ICTLab3/shopping_discount_calculator.py
+++ fp/ICTLab3/shopping_discount_calculator.py
@@ -7,9 +7,16 @@ name = "Arqam Waheed"
 cart_value = 1000 
 discount = 10 # this is percentage
 tax = 8 # this is percentage
-print(f"Name: {name} \nCartValue: {cart_value} \nDiscount: {discount}% \nTax: {tax}%")
+print("Name:", name)
+print("CartValue:", cart_value)
+print("Discount:", discount)
+print("Tax:", tax)
 
 # Task 3
 discount_amount = cart_value * discount / 100
 tax_amount = cart_value * tax / 100
 final_value = cart_value - discount_amount + tax_amount
+
+# Task 4
+
+print(f"Name: {name} \nOriginal Cart Value: {cart_value} \nDiscount applied: {discount}% \nTax amount: {tax}% \nYou need to pay {final_value}")
