ced2001390e66f82d6fd08521594f9835be75a10
ArqamWaheed <arqam.waheed.dev@gmail.com>
2025-09-26 11:27:52 +0500

    Task 5 completed

--- fp/W3Lab3/main.cpp
+++ fp/W3Lab3/main.cpp
@@ -2,15 +2,18 @@
 using namespace std;
 
 int main() {
-    float num1;
-    float num2;
-
-    cout << "Enter the number you want to increment: ";
-    cin >> num1;
-    cout << "Enter the number you want to decrement: ";
-    cin >> num2; 
-
-    cout << "\nNumber 1: " << ++num1 << endl;
-    cout << "Number 2: " << --num2 << endl;
+    int number = 0;
+    cout << "Number  Square  Cube" << endl;
+    cout << number << "       " << number * number << "       " << number * number * number << endl; number++;
+    cout << number << "       " << number * number << "       " << number * number * number << endl; number++;    
+    cout << number << "       " << number * number << "       " << number * number * number << endl; number++;    
+    cout << number << "       " << number * number << "       " << number * number * number << endl; number++;    
+    cout << number << "       " << number * number << "      " << number * number * number << endl; number++;    
+    cout << number << "       " << number * number << "      " << number * number * number << endl; number++;    
+    cout << number << "       " << number * number << "      " << number * number * number << endl; number++;    
+    cout << number << "       " << number * number << "      " << number * number * number << endl; number++;    
+    cout << number << "       " << number * number << "      " << number * number * number << endl; number++;    
+    cout << number << "       " << number * number << "      " << number * number * number << endl; number++;
+    cout << number << "      " << number * number  << "     " << number * number * number << endl;
     return 0;
 }   
