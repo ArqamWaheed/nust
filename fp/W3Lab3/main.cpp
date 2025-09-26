02040ecdd405d2e76770ae8e6a5fb58ffb8ea7ad
ArqamWaheed <arqam.waheed.dev@gmail.com>
2025-09-26 12:11:23 +0500

    Task 7

--- fp/W3Lab3/main.cpp
+++ fp/W3Lab3/main.cpp
@@ -2,18 +2,11 @@
 using namespace std;
 
 int main() {
-    int input1, input2, input3;
+    int input1, input2;
+    cout << "Enter two numbers to compare: ";
+    cin >> input1 >> input2;
     
-    cout << "Enter three integers: ";
-    cin >> input1 >> input2 >> input3;
-
-    bool pos1 = (input1 > 0);
-    bool pos2 = (input2 > 0);
-    bool pos3 = (input3 > 0);
-    
-    bool exactlyOnePositive = (pos1 && !pos2 && !pos3) || (!pos1 && pos2 && !pos3) || (!pos1 && !pos2 && pos3);
-    
-    cout << exactlyOnePositive;
+    cout << "Greater number is: " << (input1 > input2) * input1 + (input2 > input1) * input2 + (input1 == input2) * input1 << endl;
     
     return 0;
 }
