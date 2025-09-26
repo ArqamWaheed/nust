371dd02978235121e3eba9133fc741e5cd1c22eb
ArqamWaheed <arqam.waheed.dev@gmail.com>
2025-09-26 10:49:58 +0500

    W3 started, Task 1

---
+++ fp/W3Lab3/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sub1 = 80;
+    int sub2 = 70;
+    float sub3 = 87.1;
+    float sub4 = 93.4;
+    cout << "----------------------------------------------------------------" << endl;
+    cout << "                                                                " << endl;
+    cout << "                        *          *      *                     " << endl;
+    cout << "                       * *         *      *                     " << endl;
+    cout << "                      *   *        *      *                     " << endl;
+    cout << "                     *******       *  **  *                     " << endl;
+    cout << "                    *       *      * *  * *                     " << endl;
+    cout << "                   *         *     **    **                     " << endl;
+    cout << "                                                                " << endl;
+    cout << "----------------------------------------------------------------" << endl;
+    cout << "                    Subject 1:        "<< sub1 << endl;
+    cout << "                    Subject 2:        "<< sub2 << endl;
+    cout << "                    Subject 3:        "<< sub3 << endl;
+    cout << "                    Subject 4:        "<< sub4 << endl;
+    return 0;
+}   
