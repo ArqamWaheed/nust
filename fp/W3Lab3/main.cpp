9f1ca1f2e4a97db9051e512cc0d518b8eeb05004
ArqamWaheed <arqam.waheed.dev@gmail.com>
2025-09-26 12:02:57 +0500

    Task 6

--- fp/W3Lab3/main.cpp
+++ fp/W3Lab3/main.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main() {
-    int number = 0;
-    cout << "Number  Square  Cube" << endl;
-    cout << number << "       " << number * number << "       " << number * number * number << endl; number++;
-    cout << number << "       " << number * number << "       " << number * number * number << endl; number++;    
-    cout << number << "       " << number * number << "       " << number * number * number << endl; number++;    
-    cout << number << "       " << number * number << "       " << number * number * number << endl; number++;    
-    cout << number << "       " << number * number << "      " << number * number * number << endl; number++;    
-    cout << number << "       " << number * number << "      " << number * number * number << endl; number++;    
-    cout << number << "       " << number * number << "      " << number * number * number << endl; number++;    
-    cout << number << "       " << number * number << "      " << number * number * number << endl; number++;    
-    cout << number << "       " << number * number << "      " << number * number * number << endl; number++;    
-    cout << number << "       " << number * number << "      " << number * number * number << endl; number++;
-    cout << number << "      " << number * number  << "     " << number * number * number << endl;
+    int input1, input2, input3;
+    
+    cout << "Enter three integers: ";
+    cin >> input1 >> input2 >> input3;
+
+    bool pos1 = (input1 > 0);
+    bool pos2 = (input2 > 0);
+    bool pos3 = (input3 > 0);
+    
+    bool exactlyOnePositive = (pos1 && !pos2 && !pos3) || (!pos1 && pos2 && !pos3) || (!pos1 && !pos2 && pos3);
+    
+    cout << exactlyOnePositive;
+    
     return 0;
-}   
+}
