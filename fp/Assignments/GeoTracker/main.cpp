1bef81ffdde70fae262a3c51d86c512958ce75d3
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-10-11 16:31:03 +0500

    polished task 1 with input validation and task 2 completed

--- fp/Assignments/GeoTracker/main.cpp
+++ fp/Assignments/GeoTracker/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
 int main() {
@@ -10,9 +11,22 @@ int main() {
 
     cout << "Enter your username: ";
     getline(cin, username);
+
     cout << "\nEnter your current latitude and longitude respectively: ";
     cin >> inputLatitude >> inputLongitude;
+    while (cin.fail()) {
+        cout << "\ncin failed: invalid input type! Input properly again!: ";
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cin >> inputLatitude >> inputLongitude;
+    }
 
     cout << "Your username is " << username << "\nYour latitude is: " << inputLatitude << "\nYour longitude is: " << inputLongitude << endl;
+
+    // Task 2 beginning now
+
+    float distance; 
+    distance = (pow((longitude - inputLongitude), 2) + pow((latitude - inputLatitude), 2));
+    cout << "Your distance is: " << distance << endl;
     return 0;  
 }
