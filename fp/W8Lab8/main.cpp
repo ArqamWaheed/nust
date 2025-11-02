7bb8476e52c68ee1047566d98095902aa87a4f25
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-11-02 19:39:13 +0500

    Task 3

--- fp/W8Lab8/main.cpp
+++ fp/W8Lab8/main.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
-using namespace std;
 
-// Function prototype now takes float radius and returns float area
-float area(float radius);
+void calculateSquare(int &num);
 
-int main()
-{
-    float radius;
-    cout << "Enter the radius : ";
-    cin >> radius;
-    // Changed: Pass radius by value, get area back, display in main
-    float result = area(radius);
-    cout << "Area of Circle = " << result << endl;
+int main() {
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+    calculateSquare(number);
+    std::cout << "The square of the number is: " << number << std::endl;
     return 0;
 }
 
-// Function definition now takes radius parameter, calculates and returns area
-float area(float r)
-{
-    return 3.14f * r * r;
+void calculateSquare(int &num) {
+    num = num * num;
 }
