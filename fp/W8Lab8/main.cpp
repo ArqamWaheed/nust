b93e59d3ee1b2957ecf66ec06f128b02225e4531
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-11-02 19:46:06 +0500

    task 5

--- fp/W8Lab8/main.cpp
+++ fp/W8Lab8/main.cpp
@@ -1,38 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int multiply(int a, int b, int c = 1, int d = 1, int e = 1) {
-    return a * b * c * d * e;
+inline int square(int x) {
+    return x * x;
+}
+
+inline int cube(int x) {
+    return x * x * x;
 }
 
 int main() {
-    int n;
-    cout << "Enter number of integers to multiply (2-5): ";
-    cin >> n;
-    int a, b, c = 1, d = 1, e = 1;
-    cout << "Enter " << n << " integers: ";
-    cin >> a >> b;
-    if (n >= 3) cin >> c;
-    if (n >= 4) cin >> d;
-    if (n == 5) cin >> e;
-    int result;
-    switch (n) {
-        case 2:
-            result = multiply(a, b);
-            break;
-        case 3:
-            result = multiply(a, b, c);
-            break;
-        case 4:
-            result = multiply(a, b, c, d);
-            break;
-        case 5:
-            result = multiply(a, b, c, d, e);
-            break;
-        default:
-            cout << "Invalid number of integers." << endl;
-            return 1;
-    }
-    cout << "Product: " << result << endl;
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Square of the number = " << square(num) << endl;
+    cout << "Cube of the number = " << cube(num) << endl;
     return 0;
 }
