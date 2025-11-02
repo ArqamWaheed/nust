2c4d353ea30134f152fee06036561bd2e5e3649d
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-11-02 19:33:58 +0500

    Task 1

--- fp/W8Lab8/main.cpp
+++ fp/W8Lab8/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello world";
+
+void area(); // Function prototype defined here
+
+int main()
+{
+    area();
     return 0;
+}
+
+void area() // Function definition here.
+{
+    float area;
+    float radius;
+    cout << "Enter the radius : ";
+    cin >> radius;
+    area = 3.14f * radius * radius;
+    cout << "Area of Circle = " << area << endl;
 }
