b976a735d284fe4a898641d32841fc7032c074bf
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-11-02 19:41:45 +0500

    Task 4

--- fp/W8Lab8/main.cpp
+++ fp/W8Lab8/main.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
+using namespace std;
 
-void calculateSquare(int &num);
-
-int main() {
-    int number;
-    std::cout << "Enter a number: ";
-    std::cin >> number;
-    calculateSquare(number);
-    std::cout << "The square of the number is: " << number << std::endl;
-    return 0;
+int multiply(int a, int b, int c = 1, int d = 1, int e = 1) {
+    return a * b * c * d * e;
 }
 
-void calculateSquare(int &num) {
-    num = num * num;
+int main() {
+    int n;
+    cout << "Enter number of integers to multiply (2-5): ";
+    cin >> n;
+    int a, b, c = 1, d = 1, e = 1;
+    cout << "Enter " << n << " integers: ";
+    cin >> a >> b;
+    if (n >= 3) cin >> c;
+    if (n >= 4) cin >> d;
+    if (n == 5) cin >> e;
+    int result;
+    switch (n) {
+        case 2:
+            result = multiply(a, b);
+            break;
+        case 3:
+            result = multiply(a, b, c);
+            break;
+        case 4:
+            result = multiply(a, b, c, d);
+            break;
+        case 5:
+            result = multiply(a, b, c, d, e);
+            break;
+        default:
+            cout << "Invalid number of integers." << endl;
+            return 1;
+    }
+    cout << "Product: " << result << endl;
+    return 0;
 }
