124c68b8c38118d090f1d1196c4173e8031cc108
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-12-13 21:36:03 +0500

    Ignore

--- fp/W11Lab11/main.cpp
+++ fp/W11Lab11/main.cpp
@@ -2,50 +2,36 @@
 using namespace std;
 
 int main() {
-    double A[3][3];
-    double B[3][3];
-    double C[3][3];
+    int data[10];
 
-    cout << "Enter 9 elements of first 3x3 matrix (row-wise):" << endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cin >> A[i][j];
-        }
-    }
+    cout << "Enter 10 integers (space-separated or newline):" << endl;
+    for (int i = 0; i < 10; i++) cin >> data[i];
 
-    cout << "Enter 9 elements of second 3x3 matrix (row-wise):" << endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cin >> B[i][j];
-        }
-    }
+    cout << "Data items in original order" << endl;
+    for (int i = 0; i < 10; i++) cout << data[i] << (i==9?"":" ");
+    cout << endl << endl;
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            C[i][j] = 0;
-            for (int k = 0; k < 3; k++) {
-                C[i][j] += A[i][k] * B[k][j];
+    for (int pass = 1; pass <= 9; pass++) {
+        bool swapped = false;
+        for (int j = 0; j < 10 - pass; j++) {
+            if (data[j] > data[j+1]) { 
+                int tmp = data[j];
+                data[j] = data[j+1];
+                data[j+1] = tmp;
+                swapped = true;
             }
         }
+
+        cout << "After Pass " << pass << ": ";
+        for (int i = 0; i < 10; i++) cout << data[i] << (i==9?"":" ");
+        cout << endl;
+
+        if (!swapped) break; 
     }
 
-    cout << "First matrix:" << endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) cout << A[i][j] << " ";
-        cout << endl;
-    }
-
-    cout << "Second matrix:" << endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) cout << B[i][j] << " ";
-        cout << endl;
-    }
-
-    cout << "Product matrix (A * B):" << endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) cout << C[i][j] << " ";
-        cout << endl;
-    }
+    cout << endl << "Data items in ascending order" << endl;
+    for (int i = 0; i < 10; i++) cout << data[i] << (i==9?"":" ");
+    cout << endl;
 
     return 0;
 }
