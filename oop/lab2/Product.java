2417dda406e510862ce6ba5b5bebf06228f77918
ArqamWaheed <arqam.waheed.dev@gmail.com>
2026-01-26 09:52:47 +0500

    OOP Lab2 task 1

---
+++ oop/lab2/Product.java
@@ -0,0 +1,11 @@
+public class Product {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c = 3;
+
+        int product = a * b * c;
+
+        System.out.println("The product of " + a + ", " + b + ", and " + c + " is: " + product);
+    }
+}
