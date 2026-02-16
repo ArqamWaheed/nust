0ac5fdf3f646d3a503e67cb4fd2690665181f42c
ArqamWaheed <arqam.waheed.dev@gmail.com>
2026-02-16 20:10:51 +0500

    Task 5

--- oop/lab4/Task5.java
+++ oop/lab4/Task5.java
@@ -1,22 +1,74 @@
-public class Task5 
-{
-    public static void main(String[] args) 
-    {
-        System.out.println("All combinations pick(2 from 1-7):");
-        
-        int counter = 0;
-        int first = 1;
-        
-        while(first <= 7) {
-            int second = first + 1;
-            while(second <= 7) {
-                System.out.println("(" + first + ", " + second + ")");
-                counter++;
-                second++;
-            }
-            first++;
+import java.util.Scanner;
+
+class QuadraticEquation {
+    double a, b, c;
+
+    QuadraticEquation(double a1, double b1, double c1) {
+        a = a1;
+        b = b1;
+        c = c1;
+    }
+
+    double getA() { 
+        return a; 
+    }
+    double getB() { 
+        return b; 
+    }
+    double getC() { 
+        return c; 
+    }
+
+    double calcDiscriminant() {
+        return b * b - 4 * a * c;
+    }
+
+    double calcRoot1() {
+        double disc = calcDiscriminant();
+        if(disc < 0)
+            return 0;
+        else
+            return (-b + Math.sqrt(disc)) / (2 * a);
+    }
+
+    double calcRoot2() {
+        double disc = calcDiscriminant();
+        if(disc < 0)
+            return 0;
+        else
+            return (-b - Math.sqrt(disc)) / (2 * a);
+    }
+}
+
+public class Task5 {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter value of a: ");
+        double a = scan.nextDouble();
+        System.out.print("Enter value of b: ");
+        double b = scan.nextDouble();
+        System.out.print("Enter value of c: ");
+        double c = scan.nextDouble();
+
+        QuadraticEquation eq = new QuadraticEquation(a, b, c);
+
+        double disc = eq.calcDiscriminant();
+
+        System.out.println();
+        if(disc > 0) {
+            System.out.println("Two roots exist:");
+            System.out.println("Root 1 = " + eq.calcRoot1());
+            System.out.println("Root 2 = " + eq.calcRoot2());
         }
-        
-        System.out.println("Total number of combinations: " + counter);
+        else if(disc == 0) {
+            System.out.println("One root exists:");
+            System.out.println("Root = " + eq.calcRoot1());
+        }
+        else {
+            System.out.println("The equation has no roots.");
+        }
+
+        scan.close();
     }
 }
