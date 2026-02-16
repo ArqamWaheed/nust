e6f988bb157f023cd58e3bdedabd42070547a624
ArqamWaheed <arqam.waheed.dev@gmail.com>
2026-02-16 20:10:43 +0500

    Task 4

--- oop/lab4/Task4.java
+++ oop/lab4/Task4.java
@@ -1,38 +1,85 @@
-import java.util.Scanner;
+class Stack {
+    int arr[];
+    int top;
 
-public class Task4 
-{
-    public static void main(String[] args) 
-    {
-        Scanner scan = new Scanner(System.in);
-        
-        System.out.print("Enter loan amount: ");
-        double loan = scan.nextDouble();
-        
-        System.out.print("Enter number of years: ");
-        int numYears = scan.nextInt();
-        
-        if(loan <= 0 || numYears <= 0) {
-            System.out.println("Invalid input! Exiting...");
-            scan.close();
-            return;
+    Stack() {
+        arr = new int[10];
+        top = -1;
+    }
+
+    void push(int val) {
+        if(top >= 9) {
+            System.out.println("Stack Overflow!");
         }
-        
-        System.out.println("Interest Rate    Monthly Payment    Total Payment");
-        
-        double interestRate = 5.0;
-        while(interestRate <= 10.0) {
-            double monthlyInterestRate = interestRate / 1200.0;
-            int totalMonths = numYears * 12;
-            
-            double monthlyPay = (loan * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -totalMonths));
-            double totalPay = monthlyPay * totalMonths;
-            
-            System.out.printf("%.2f%%           $%.2f            $%.2f\n", interestRate, monthlyPay, totalPay);
-            
-            interestRate = interestRate + 0.25;
+        else {
+            top++;
+            arr[top] = val;
         }
-        
-        scan.close();
+    }
+
+    int pop() {
+        if(top < 0) {
+            System.out.println("Stack Underflow!");
+            return -1;
+        }
+        else {
+            int val = arr[top];
+            top--;
+            return val;
+        }
+    }
+
+    void display() {
+        if(top < 0) {
+            System.out.println("Empty Stack");
+        }
+        else {
+            System.out.print("Current Stack: ");
+            int i = 0;
+            while(i <= top) {
+                System.out.print(arr[i] + " ");
+                i++;
+            }
+            System.out.println();
+        }
+    }
+}
+
+public class Task4 {
+    public static void main(String args[]) {
+        Stack s = new Stack();
+
+        System.out.println("Adding 5 elements: 15, 25, 35, 45, 55");
+        s.push(15);
+        s.push(25);
+        s.push(35);
+        s.push(45);
+        s.push(55);
+        s.display();
+
+        System.out.println();
+        int popped = s.pop();
+        System.out.println("Popped: " + popped);
+        s.display();
+
+        System.out.println();
+        popped = s.pop();
+        System.out.println("Popped: " + popped);
+        s.display();
+
+        System.out.println();
+        System.out.println("Adding 65, 75");
+        s.push(65);
+        s.push(75);
+        s.display();
+
+        System.out.println();
+        System.out.println("Testing overflow by adding more elements:");
+        s.push(85);
+        s.push(95);
+        s.push(105);
+        s.push(115);
+        s.push(125);
+        s.display();
     }
 }
